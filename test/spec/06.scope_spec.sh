ff1bb81cf68cd9d0ecf80e157460cbd8189be22a
Ismo Vuorinen <ismo@ivuorinen.net>
2025-09-13 03:54:32 +0300

    fix: apply CodeRabbit nitpick suggestions and improve code quality
    - Fix grammar.js TypeScript errors by correcting optional field usage
    - Update .yamlignore to use more robust glob pattern (**/node_modules/**)
    - Remove hard-coded test count from README.md for maintainability
    - Fix shellcheck directive format (add space after #) in all test specs
    - Fix typos throughout test specifications:
      - 'can not' → 'cannot'
      - 'expantion' → 'expansion'
      - 'singnal' → 'signal'
      - 'It mean' → 'It means'
    - Update CODE_OF_CONDUCT.md HTTP links to HTTPS
    - Update tree-sitter parse command to use --scope instead of --language
    - Add comments to .mega-linter.yml explaining disabled linters
    
    All grammar tests still pass (61/61) and the parser functions correctly
    with the updated tree-sitter CLI v0.25.0.

--- test/spec/06.scope_spec.sh
+++ test/spec/06.scope_spec.sh
@@ -1,49 +1,49 @@
-#shellcheck shell=sh
+# shellcheck shell=sh
 
 # Each block (example group / example) runs within subshell.
-# It mean that it works like lexical scope.
+# It means that it works like lexical scope.
 
 Describe 'scope example'
-  foo() { echo "foo"; } # It can call from anywhere within this example group
+foo() { echo "foo"; } # It can call from anywhere within this example group
 
-  # By the way, you can only use shellspec DSL or define function here.
-  # Of course it is possible to write freely within the defined function
-  # but other code may breaks isolation of tests.
+# By the way, you can only use shellspec DSL or define function here.
+# Of course it is possible to write freely within the defined function
+# but other code may breaks isolation of tests.
 
-  It 'calls "foo"'
-    When call foo
-    The output should eq 'foo'
-  End
+It 'calls "foo"'
+When call foo
+The output should eq 'foo'
+End
 
-  It 'defines "bar" function'
-    bar() { echo "bar"; }
-    When call bar
-    The output should eq 'bar'
-  End
+It 'defines "bar" function'
+bar() { echo "bar"; }
+When call bar
+The output should eq 'bar'
+End
 
-  It 'can not call "bar" function, because different scope'
-    When call bar
-    The status should be failure # probably status is 127
-    The stderr should be present # probably stderr is "bar: not found"
-  End
+It 'cannot call "bar" function, because different scope'
+When call bar
+The status should be failure # probably status is 127
+The stderr should be present # probably stderr is "bar: not found"
+End
 
-  It 'redefines "foo" function'
-    foo() { echo "FOO"; }
-    When call foo
-    The output should eq 'FOO'
-  End
+It 'redefines "foo" function'
+foo() { echo "FOO"; }
+When call foo
+The output should eq 'FOO'
+End
 
-  It 'calls "foo" function of outer scope (not previous example)'
-    When call foo
-    The output should eq 'foo'
-  End
+It 'calls "foo" function of outer scope (not previous example)'
+When call foo
+The output should eq 'foo'
+End
 
-  Describe 'sub block'
-    foo() { echo "Foo"; }
+Describe 'sub block'
+foo() { echo "Foo"; }
 
-    It 'calls "foo" function of upper scope'
-      When call foo
-      The output should eq 'Foo'
-    End
-  End
+It 'calls "foo" function of upper scope'
+When call foo
+The output should eq 'Foo'
+End
+End
 End
