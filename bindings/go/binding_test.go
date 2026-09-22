0fdfdbf6a3df1064cdf8dd4dcbbba127baffc2c9
Ismo Vuorinen <ismo@ivuorinen.net>
2026-09-22 20:49:06 +0200

    style(go): sort binding test imports for gofmt

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,8 +3,8 @@ package tree_sitter_shellspec_test
 import (
 	"testing"
 
-	tree_sitter "github.com/tree-sitter/go-tree-sitter"
 	tree_sitter_shellspec "github.com/ivuorinen/tree-sitter-shellspec/bindings/go"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
